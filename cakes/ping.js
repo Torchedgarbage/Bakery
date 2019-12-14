79121fd1c604d5dd3c74d6490dd6a89d165ab7ad
SamuraiStacks <samuraistacksmmlol@gmail.com>
2019-12-14 08:46:55 -0400

    fixed up and finished ping command

--- cakes/ping.js
+++ cakes/ping.js
@@ -1,6 +1,14 @@
 exports.command = {
     run: (bakery, msg) => {
-        msg.channel.createMessage("Pong!")
+        msg.channel.createMessage({embed: {
+            color: 0xFE0060,
+            description: `🏓 Pong! \`${bakery.shards.get(0).latency}\`ms
+                          🏘️ In \`${bakery.guilds.size}\` guilds!`,
+            author: {
+                name: "Bakery - Ping",
+                icon_url: bakery.user.avatarURL
+            }
+        }})
     },
     config: {
         aliases: []
