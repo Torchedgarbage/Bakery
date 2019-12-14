54822b23df57b32ef7ccda74dc20909af698dbaf
SamuraiStacks <samuraistacksmmlol@gmail.com>
2019-12-14 11:10:32 -0400

    added footer

--- cakes/help.js
+++ cakes/help.js
@@ -1,5 +1,6 @@
 exports.command = {
     run: (bakery, msg, args, prefix) => {
+        const owner = bakery.users.get("439373663905513473")
         const cmds = bakery.commands.filter(c => !c.command.config.ownerOnly).map(c => `\`${prefix}${c.command.help.usage}\` **--** ${c.command.help.desc}`).join("\n")
         if(!args[0]) {
             msg.channel.createMessage({embed: {
@@ -11,6 +12,10 @@ exports.command = {
                 },
                 thumbnail: {
                     url: bakery.user.avatarURL.replace("jpg", "png")
+                },
+                footer: {
+                    text: `Requested by ${msg.author.username}#${msg.author.discriminator} | ©️ 2020 ${owner.username}#${owner.discriminator}`,
+                    icon_url: msg.author.avatarURL
                 }
             }})
         } else {
